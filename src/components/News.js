5545784cf1530d628ed16c4370e4dc61106635b9
Shivam Kumar <syndaxlevenis@gmail.com>
2022-04-07 01:16:07 +0530

    newsmonkey buttons

---
+++ src/components/News.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
+export class News extends Component {
+
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general',
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
+  capitalizedFirstLetter = (string)=> {
+      return string.charAt(0).toUpperCase()+string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1
+    }
+    document.title = `${this.capitalizedFirstLetter(this.props.category)} - NewsMonkey`;
+  }
+
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=97ea130964b849a88ce3f90f19674306&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({
+      articles: parsedData.articles,
+      totalArticles: parsedData.totalResults,
+      loading: false
+    })
+  }
+
+  async componentDidMount() {
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=97ea130964b849a88ce3f90f19674306&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    // this.setState({ loading: true })
+    // let data = await fetch(url);
+    // let parsedData = await data.json()
+    // this.setState({
+    //   articles: parsedData.articles,
+    //   totalArticles: parsedData.totalResults,
+    //   loading: false
+    // })
+    this.updateNews();
+  }
+
+  handlePrevClick = async () => {
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=97ea130964b849a88ce3f90f19674306&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    // this.setState({ loading: true })
+    // let data = await fetch(url);
+    // let parsedData = await data.json()
+    // this.setState({
+    //   page: this.state.page - 1,
+    //   articles: parsedData.articles,
+    //   loading: false
+    // })
+    this.setState({page: this.state.page - 1});
+    this.updateNews();
+  }
+
+  handleNextClick = async () => {
+    // if (!(this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize))) {
+    //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=97ea130964b849a88ce3f90f19674306&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    //   this.setState({ loading: true })
+    //   let data = await fetch(url);
+    //   let parsedData = await data.json()
+    //   this.setState({
+    //     page: this.state.page + 1,
+    //     articles: parsedData.articles,
+    //     loading: false
+    //   })
+    // }
+    this.setState({page: this.state.page + 1});
+    this.updateNews();
+  }
+
+  render() {
+    return (
+      <div className='container my-3'>
+        <h1 className='text-center' style={{ margin: '35 px' }}>NewsMonkey - Top {this.props.category!='general'?this.capitalizedFirstLetter(this.props.category):''} Headlines</h1>
+        {this.state.loading && <Spinner />}
+        <div className="row">
+          {!this.state.loading && this.state.articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title ? element.title.slice(0, 45) : " "} description={element.description ? element.description.slice(0, 80) : " "} imgUrl={element.urlToImage ? element.urlToImage : "https://c.ndtvimg.com/2022-04/0pbr5a78_kyiv-bucha-afp_625x300_03_April_22.jpg"} newsUrl={element.url} author={element.author ? element.author : 'Unknown'} date={element.publishedAt} source={element.source.name} />
+            </div>
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} type='button' className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize)} type='button' className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default News
